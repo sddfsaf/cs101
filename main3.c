f2d2ca824fd4211d3745e59e139d806c40f76741
sddfsaf <120005665+sddfsaf@users.noreply.github.com>
2022-12-27 17:10:56 +0800

    Add files via upload

---
+++ main3.c
@@ -0,0 +1,19 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    int year =2022;
+    if(year%4 == 0 && year%100 != 0){
+        printf("是閏年");
+    }else{
+       if(year%400 == 0){
+         printf("是閏年");
+       }else{
+         printf("不是閏年");
+       }
+    }
+
+    return 0;
+}
